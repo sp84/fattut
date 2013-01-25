57b9ae6d35d71cb9178b3ac15d489063cd78fea5
Sushil Poudyal <sushil.poudyal@gmail.com>
2013-01-24 19:34:06 -0500

    Laravel Base

--- lib/db.php
+++
@@ -1,8 +0,0 @@
-<?php 
-$dbhost = 'localhost';
-$dbname = 'fattut';
-$dbuser = 'root';
-$dbpass = '';
-$conn = mysql_connect($dbhost, $dbuser, $dbpass);
-mysql_select_db($dbname, $conn);
-?>
--- lib/logout.php
+++
@@ -1,6 +0,0 @@
-<?php
-session_start();
-require('functions.php');
-logout();
-header('location: ../login_form.php');
-?>
